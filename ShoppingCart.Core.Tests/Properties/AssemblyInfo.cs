fa4b5a5c8cdbe8482319f70ec5b982e8f2f2bfdd
Mohamed Romdhani <mromdhani7@gmail.com>
2018-11-08 17:53:00 +0100

    Commit de la préparation des layers

---
+++ ShoppingCart.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShoppingCart.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShoppingCart.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c5b5001-1a4f-41f3-ae2b-c7cb75ee0e6c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ShoppingCart.Core/Domain/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Core.Domain
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Photo")]
+        public string PhotoFile { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {      
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            return ((Product)obj).ProductId.Equals(ProductId);
+         
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return ProductId.GetHashCode();
+        }
+    }
+}
