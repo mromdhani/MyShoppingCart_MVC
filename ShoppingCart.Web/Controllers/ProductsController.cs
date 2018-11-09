bb806691aa800e12caa561254e7dc6cea017aeb2
Mohamed Romdhani <mromdhani7@gmail.com>
2018-11-09 10:37:23 +0100

    Terminaison de la gestion du panier

--- ShoppingCart.Web/Controllers/ProductsController.cs
+++ ShoppingCart.Web/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using ShoppingCart.Core.Facade;
+﻿using ShoppingCart.Core.Domain;
+using ShoppingCart.Core.Facade;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,40 @@ namespace ShoppingCart.Web.Controllers
         {
             return View(repo.FindAll());
         }
+
+        public ActionResult AddToCart(string id)
+        {
+
+            List<Item> panier = null;
+            Product produit = repo.FindAll().FirstOrDefault(p => p.ProductId.Equals(id));
+            if (Session["cart"] ==  null)
+            {
+                panier = new List<Item> {
+                    new Item { Product= produit, Quantity=1} 
+                };
+                Session["cart"] = panier;
+
+            }else
+            {
+                panier = (List<Item>)Session["cart"];
+
+                Item p = panier.Find(i => i.Product.ProductId.Equals(id));
+           
+                if (p == null )  // Le produit n'existe pas dans le panier
+                {
+                    panier.Add(new Item {Product=produit, Quantity=1 });
+                    Session["cart"] = panier;
+                }
+                else
+                {
+                    p.Quantity++;
+                    Session["cart"] = panier;
+
+                }
+            }
+            return View("Panier", panier);
+        }
+
 
         public ActionResult About()
         {
